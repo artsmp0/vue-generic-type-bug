f2ca4b9d710ed40dd22a8e01dac80d9f91beb894
沈孟平 <shen990206@gmail.com>
2023-05-12 17:50:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const count = ref(100);
+</script>
+
+<template>
+  {{ count }}
+  <HelloWorld :msg="'hello world'">
+    <template #default="{ msg, count }">
+      <div>
+        {{ msg }}
+        {{ count }}
+      </div>
+    </template>
+  </HelloWorld>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts" generic="T extends string">
+import { computed, ref } from 'vue';
+
+const props = defineProps<{
+  msg: T;
+}>();
+
+const newMsg = computed<T>(() => props.msg);
+
+defineSlots<{
+  default?: (props: { msg: T; count: number }) => any;
+}>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+  <div>
+    <slot name="default" :msg="newMsg" :count="count"> </slot>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
